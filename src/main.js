38073972638dc5e36e44c75a7b2af243e991ec9a
Oscar Romero <dev.omrc@gmail.com>
2019-04-24 00:47:44 -0600

    Config main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./../node_modules/bulma/css/bulma.min.css";
 
-Vue.config.productionTip = false
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+    faPlus,
+    faClock,
+    faCheck,
+    faTrash
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import router from "./router";
+
+library.add([faPlus, faClock, faCheck, faTrash]);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount("#app");
