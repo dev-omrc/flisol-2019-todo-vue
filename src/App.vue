6bc1796fd9f58a342cdc3e201f7238e6908b0f9a
Oscar Romero <dev.omrc@gmail.com>
2019-04-24 00:49:19 -0600

    Update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-navbar :auth="this.auth" @loggedOut="logout"></app-navbar>
+    <router-view @authenticated="setAuth"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appNavbar from "./components/Navbar";
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      auth: false
+    };
+  },
   components: {
-    HelloWorld
+    appNavbar
+  },
+  methods: {
+    setAuth() {
+      this.auth = true;
+    },
+    logout() {
+      this.auth = false;
+    }
+  },
+  mounted() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      this.token = token;
+      this.auth = true;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
